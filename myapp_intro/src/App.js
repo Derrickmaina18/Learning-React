caa42ee00665c16e51190740d74c9995d3a55252
Derrick Maina <derrickmaina@Derricks-MacBook-Air.local>
2022-09-19 08:18:01 +0300

    First output

--- myapp_intro/src/App.js
+++ myapp_intro/src/App.js
@@ -2,23 +2,22 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const user = {
+    name : 'Derrick',
+    age : 17,
+  }  
+  const user2 = 'Maina'
   return (
+    <>
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <p>Hi my name is {user.name} and I'm {user.age} years old</p>
     </div>
+    <div>
+      <p className = "textp">
+        user2
+      </p>
+    </div>
+    </>
   );
 }
 
