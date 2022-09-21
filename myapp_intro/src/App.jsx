563cfeaf06d43da398879f61a808dd43a87d9944
Derrick Maina <derrickmaina@Derricks-MacBook-Air.local>
2022-09-21 08:01:28 +0300

    Conditional Rendering

--- myapp_intro/src/App.jsx
+++ myapp_intro/src/App.jsx
@@ -4,11 +4,17 @@ import Card from "./components/Card/Card";
 import Spacer from "./components/Card/Spacer";
 
 function App() {
-  const [count, setCount] = useState(0);
+  const user = {
+    name: "Derrick",
+    isAuth: false,
+  }
+  const {name, isAuth} = user
   return (
     <>
-      <button onClick={() => setCount(count + 1)}>Counter Add</button>
-      <p>{count}</p>
+      <h1>The App</h1>
+      <p className={isAuth ? "isAuthenticated" : "isUnAuth"}>Welcome {isAuth ? name : "Please Login First"}</p>
+
+      {!isAuth && <button type="button">Login</button>}
     </>
   );
 }
